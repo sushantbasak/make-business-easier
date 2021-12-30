49fbaa52c31d4ef170b85f9c595a4798e332ab28
sushantbasak <sushantbasak73@gmail.com>
2021-12-30 16:34:38 +0530

    fix: Env Export Variables

--- config/index.js
+++ config/index.js
@@ -1,12 +1,27 @@
 'use strict';
 
+require('dotenv').config();
+
 const config = {
   development: {
-    mongoDb: 'mongodb://127.0.0.1:27017/bussiness-api',
-    port: process.env.PORT || 8001,
+    mongoDb: process.env.DB_URL,
+    port: process.env.PORT || 3001,
+    saltRound: process.env.SALTROUND,
+    jwt: {
+      secret: process.env.JWT_SECRET,
+      resetsecret: process.env.JWT_SECRET_RESET,
+      expiresIn: process.env.EXPIRESIN,
+      resetexpiresIn: process.env.RESETEXPIRESIN,
+    },
+    reactUrl: process.env.REACT_APP_URL,
+    serverUrl: process.env.SERVER_URL,
+    apiUrl: process.env.API_URL,
+    homeMail: process.env.HOME_MAIL,
+    homeMailPassword: process.env.HOME_MAIL_PASSWORD,
+    emailServiceProvider: process.env.EMAIL_SERVICE_PROVIDER,
   },
 };
 
-const appSettings = { ...config[process.env.NODE_ENV || 'development'] };
+const appSettings = { ...config['development'] };
 
 module.exports = appSettings;
