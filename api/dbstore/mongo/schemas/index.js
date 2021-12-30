e77e041eb6846f6dd1887052ec7fd8b4ea9d982b
sushantbasak <sushantbasak73@gmail.com>
2021-12-30 12:00:15 +0530

    feat: Add User Schema

---
+++ api/dbstore/mongo/schemas/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const User = require('./user');
+
+const schemas = {
+  User,
+};
+
+module.exports = schemas;
---
+++ api/dbstore/mongo/schemas/user.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const { COLLECTIONS } = require('../../../../constants');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'Please add First name'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Please add Last name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number can not be longer than 20 characters'],
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+  },
+  isEmailConfirmed: {
+    type: Boolean,
+    default: false,
+  },
+  isPasswordReset: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = {
+  schema: mongoose.model(COLLECTIONS.USER, userSchema),
+};
