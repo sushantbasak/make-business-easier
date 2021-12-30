da42977497ddd4be125189674b939a652ba6d36f
sushantbasak <sushantbasak73@gmail.com>
2021-12-30 12:11:40 +0530

    feat: Add User Service

---
+++ api/v1/services/userService.js
@@ -0,0 +1,60 @@
+// Services
+
+const dbStoreHandler = require('../../dbstore/mongo');
+
+// Functions
+
+const createUser = async (data) => {
+  const { result, hasError } = await dbStoreHandler.createUser(data);
+
+  if (hasError || result == null) {
+    return { status: 'ERROR_FOUND' };
+  }
+  return { result, status: 'USER_CREATED' };
+};
+
+const findUser = async (data) => {
+  const { result, hasError } = await dbStoreHandler.findUser(data);
+
+  if (hasError) {
+    return { status: 'ERROR_FOUND' };
+  }
+
+  if (result === null) return { status: 'NOT_FOUND' };
+
+  return { result, status: 'USER_FOUND' };
+};
+
+const getPassword = async (data) => {
+  const { result, hasError } = await dbStoreHandler.getPassword(data);
+
+  if (result === null) return { status: 'NOT_FOUND' };
+
+  if (hasError) {
+    return { status: 'ERROR_FOUND' };
+  }
+  return { result, status: 'USER_FOUND' };
+};
+
+const updateUser = async (filter, updateData) => {
+  const { result, hasError } = await dbStoreHandler.updateUser(
+    filter,
+    updateData
+  );
+
+  if (result === null) return { status: 'NOT_FOUND' };
+
+  if (hasError) {
+    return { status: 'ERROR_FOUND' };
+  }
+  return { result, status: 'USER_UPDATED' };
+};
+
+const userService = {
+  createUser,
+  findUser,
+  getPassword,
+  updateUser,
+};
+
+module.exports = userService;
