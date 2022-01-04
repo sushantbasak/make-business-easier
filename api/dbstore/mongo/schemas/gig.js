62083e4bdf4054a0502ede8cf3b545e7bbf6243f
SamarpreetSingh <samar.guraya18@gmail.com>
2022-01-04 23:49:21 +0530

    feat: add gig model

---
+++ api/dbstore/mongo/schemas/gig.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const {COLLECTIONS} = require('../../../../constants');
+
+const gigSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    title: {
+        type: String,
+        required: [true, "Please add gig title"],
+        maxLength: [50, "Please add title shorter than 50 characters"]
+    },
+    category: {
+        type: String,
+        default: 'general'
+    },
+    about: {
+        type: String
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    price: {
+        type: Number
+    },
+    picture: {
+        type: String,
+        default: "picture.jpg"
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model(COLLECTIONS.GIG, gigSchema);
