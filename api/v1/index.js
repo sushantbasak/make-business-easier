13219b4e99b0e4bd4a9aefb1aa7fe1d10cfd32cb
sushantbasak <sushantbasak73@gmail.com>
2022-01-04 20:44:27 +0530

    fix: Auth Middleware & Router Export

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.use('/user', require('./controllers/userController'));
+router.use('/auth', require('./controllers/authController'));
 
 router.get('/health', (req, res) => {
   res.send({ status: 'UP' });
