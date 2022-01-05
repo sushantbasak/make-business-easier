f605e2a57a732537ac5634eec6b56bfd9e2fb63d
SamarpreetSingh <samar.guraya18@gmail.com>
2022-01-06 01:01:34 +0530

    feat: add message queries

---
+++ api/dbstore/mongo/queries/message.js
@@ -0,0 +1,54 @@
+const { Message } = require('../schemas');
+const ErrorHandler = require('../../../utils/errorHandler');
+
+const createMessage = async (body) => {
+    try {
+        const message = await Message.schema.create(body);
+        const result = message ? message.toJSON() : null;
+        return {result: result, hasError: null}
+    } catch(err) {
+        ErrorHandler.extractError(err);
+        return {result: null, hasError: true};
+    }
+}
+
+const findMessage = async (body) => {
+  try {
+      const message = await Message.schema.findOne(body);
+      const result = message ? message.toJSON() : null;
+      return {result: result, hasError: null};
+  } catch(err) {
+      ErrorHandler.extractError(err);
+      return {result: null, hasError: true};
+        
+  }
+}
+
+const findAllMessage = async (body) => {
+  try {
+      const message = await Message.schema.find(body);
+      return {result: message, hasError: null};
+  } catch(err) {
+      ErrorHandler.extractError(err);
+      return {result: null, hasError: true};
+        
+  }
+}
+
+const updateMessage = async (filter, update) => {
+    try {
+        if(!update){
+            return {result: null, hasError: null}
+        }
+        const message = await Message.schema.findOneAndUpdate(filter, update, {
+            new: true,
+            runValidators: true
+        });
+        return {result: message, hasError: null}
+    } catch(err) {
+        ErrorHandler.extractError(err);
+        return {result: null, hasError: true}
+    } 
+}
+
+module.exports = { createMessage, findMessage, findAllMessage, updateMessage }
