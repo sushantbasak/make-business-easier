7f3259c7da088dcd726ff17c6c60965ea7db32dc
ImAkhil24 <akhilbadoni24@gmail.com>
2022-01-06 22:48:48 +0530

    refactor: admin middleware

--- api/v1/middleware/admin.js
+++ api/v1/middleware/admin.js
@@ -1,9 +1,9 @@
 /* eslint-disable consistent-return */
-const httpCode = require('http-status-codes');
-const { MESSAGES } = require('../../../constants');
+const httpCode = require("http-status-codes");
+const { MESSAGES, ROLES } = require("../../../constants");
 
 const adminProtect = (req, res, next) => {
-  if (req.user.role !== 'admin' )
+  if (!(req.user.role === ROLES.ADMIN || req.user.role === ROLES.ROOT))
     return res.sendError(
       httpCode.StatusCodes.UNAUTHORIZED,
       MESSAGES.api.UNAUTHORIZED_USER
