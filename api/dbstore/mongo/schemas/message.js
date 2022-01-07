d7be665ee4339237fdbae160a574a64b786d3bdf
sushantbasak <sushantbasak73@gmail.com>
2022-01-07 12:33:42 +0530

    fix: Linting Errors

--- api/dbstore/mongo/schemas/message.js
+++ api/dbstore/mongo/schemas/message.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const { COLLECTIONS } = require('../../../../constants');
 
 const messageSchema = new mongoose.Schema({
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: COLLECTIONS.USER
-    },
-    content: {
-        type: String
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    }
-})
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: COLLECTIONS.USER,
+  },
+  content: {
+    type: String,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = {
-  schema: mongoose.model(COLLECTIONS.MESSAGE, messageSchema)
+  schema: mongoose.model(COLLECTIONS.MESSAGE, messageSchema),
 };
--- api/dbstore/mongo/schemas/order.js
+++ api/dbstore/mongo/schemas/order.js
@@ -2,35 +2,35 @@ const mongoose = require('mongoose');
 const { COLLECTIONS } = require('../../../../constants');
 
 const orderSchema = new mongoose.Schema({
-    buyer: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: COLLECTIONS.USER,
-        required: [true, "Please add a buyer"]
-    },
-    seller: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: COLLECTIONS.USER,
-        required: [true, "Please add a seller"]
-    },
-    gig: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: COLLECTIONS.GIG,
-        required: [true, "Please add the gig"]
-    },
-    status: {
-        type: String,
-        enum: ['pending', 'inprogress', 'completed', 'rejected'],
-        default: 'pending'
-    },
-    messages: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: COLLECTIONS.MESSAGE
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    }
-})
+  buyer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: COLLECTIONS.USER,
+    required: [true, 'Please add a buyer'],
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: COLLECTIONS.USER,
+    required: [true, 'Please add a seller'],
+  },
+  gig: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: COLLECTIONS.GIG,
+    required: [true, 'Please add the gig'],
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'inprogress', 'completed', 'rejected'],
+    default: 'pending',
+  },
+  messages: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: COLLECTIONS.MESSAGE,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = {
   schema: mongoose.model(COLLECTIONS.ORDER, orderSchema),
