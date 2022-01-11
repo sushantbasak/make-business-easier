1e77e42ba186f2fdd8280879e5506c545812b60a
SamarpreetSingh <samar.guraya18@gmail.com>
2022-01-11 22:38:39 +0530

    style: add prettier config

--- api/v1/controllers/gigController.js
+++ api/v1/controllers/gigController.js
@@ -1,30 +1,23 @@
-const router = require("express").Router();
-const httpCode = require("http-status-codes");
+const router = require('express').Router();
+const httpCode = require('http-status-codes');
 
-const ErrorHandler = require("../../utils/errorHandler");
-const gigService = require("../services/gigService");
-const { protect } = require("../middleware/auth");
-const { MESSAGES } = require("../../../constants");
+const ErrorHandler = require('../../utils/errorHandler');
+const gigService = require('../services/gigService');
+const { protect } = require('../middleware/auth');
+const { MESSAGES } = require('../../../constants');
 
 const createGig = async (req, res) => {
   try {
     const result = await gigService.createGig(req.body);
 
-    if (result.status === "ERROR_FOUND") {
-      throw new Error("Unable to create new Gig");
+    if (result.status === 'ERROR_FOUND') {
+      throw new Error('Unable to create new Gig');
     }
 
-    return res.sendSuccess(
-      result.result,
-      MESSAGES.api.CREATED,
-      httpCode.StatusCodes.CREATED
-    );
+    return res.sendSuccess(result.result, MESSAGES.api.CREATED, httpCode.StatusCodes.CREATED);
   } catch (err) {
     ErrorHandler.extractError(err);
-    return res.sendError(
-      httpCode.StatusCodes.INTERNAL_SERVER_ERROR,
-      MESSAGES.api.SOMETHING_WENT_WRONG
-    );
+    return res.sendError(httpCode.StatusCodes.INTERNAL_SERVER_ERROR, MESSAGES.api.SOMETHING_WENT_WRONG);
   }
 };
 
@@ -32,27 +25,17 @@ const getGig = async (req, res) => {
   try {
     const result = await gigService.findGig({ _id: req.params.gig_id });
 
-    if (result.status === "ERROR_FOUND") {
-      throw new Error("Unable to get the Gig");
+    if (result.status === 'ERROR_FOUND') {
+      throw new Error('Unable to get the Gig');
     }
-    if (result.status === "NOT_FOUND") {
-      return res.sendError(
-        httpCode.StatusCodes.BAD_REQUEST,
-        MESSAGES.api.NOT_FOUND
-      );
+    if (result.status === 'NOT_FOUND') {
+      return res.sendError(httpCode.StatusCodes.BAD_REQUEST, MESSAGES.api.NOT_FOUND);
     }
 
-    return res.sendSuccess(
-      result.result,
-      MESSAGES.api.FOUND,
-      httpCode.StatusCodes.FOUND
-    );
+    return res.sendSuccess(result.result, MESSAGES.api.FOUND, httpCode.StatusCodes.FOUND);
   } catch (err) {
     ErrorHandler.extractError(err);
-    return res.sendError(
-      httpCode.StatusCodes.INTERNAL_SERVER_ERROR,
-      MESSAGES.api.SOMETHING_WENT_WRONG
-    );
+    return res.sendError(httpCode.StatusCodes.INTERNAL_SERVER_ERROR, MESSAGES.api.SOMETHING_WENT_WRONG);
   }
 };
 
@@ -60,58 +43,35 @@ const getAllGigs = async (req, res) => {
   try {
     const result = await gigService.findAllGig(req.body);
 
-    if (result.status === "ERROR_FOUND") {
-      throw new Error("Unable to get all Gigs");
+    if (result.status === 'ERROR_FOUND') {
+      throw new Error('Unable to get all Gigs');
     }
-    if (result.status === "NOT_FOUND") {
-      return res.sendError(
-        httpCode.StatusCodes.BAD_REQUEST,
-        MESSAGES.api.NOT_FOUND
-      );
+    if (result.status === 'NOT_FOUND') {
+      return res.sendError(httpCode.StatusCodes.BAD_REQUEST, MESSAGES.api.NOT_FOUND);
     }
 
-    return res.sendSuccess(
-      result.result,
-      MESSAGES.api.FOUND,
-      httpCode.StatusCodes.FOUND
-    );
+    return res.sendSuccess(result.result, MESSAGES.api.FOUND, httpCode.StatusCodes.FOUND);
   } catch (err) {
     ErrorHandler.extractError(err);
-    return res.sendError(
-      httpCode.StatusCodes.INTERNAL_SERVER_ERROR,
-      MESSAGES.api.SOMETHING_WENT_WRONG
-    );
+    return res.sendError(httpCode.StatusCodes.INTERNAL_SERVER_ERROR, MESSAGES.api.SOMETHING_WENT_WRONG);
   }
 };
 
 const updateGig = async (req, res) => {
   try {
-    const result = await gigService.updateGig(
-      { _id: req.params.gig_id },
-      req.body
-    );
+    const result = await gigService.updateGig({ _id: req.params.gig_id }, req.body);
 
-    if (result.status === "ERROR_FOUND") {
-      throw new Error("Unable to update the Gig");
+    if (result.status === 'ERROR_FOUND') {
+      throw new Error('Unable to update the Gig');
     }
-    if (result.status === "NOT_FOUND") {
-      return res.sendError(
-        httpCode.StatusCodes.BAD_REQUEST,
-        MESSAGES.api.NOT_FOUND
-      );
+    if (result.status === 'NOT_FOUND') {
+      return res.sendError(httpCode.StatusCodes.BAD_REQUEST, MESSAGES.api.NOT_FOUND);
     }
 
-    return res.sendSuccess(
-      result.result,
-      MESSAGES.api.UPDATE_SUCCESSFULL,
-      httpCode.StatusCodes.OK
-    );
+    return res.sendSuccess(result.result, MESSAGES.api.UPDATE_SUCCESSFULL, httpCode.StatusCodes.OK);
   } catch (err) {
     ErrorHandler.extractError(err);
-    return res.sendError(
-      httpCode.StatusCodes.INTERNAL_SERVER_ERROR,
-      MESSAGES.api.UPDATE_UNSUCCESSFULL
-    );
+    return res.sendError(httpCode.StatusCodes.INTERNAL_SERVER_ERROR, MESSAGES.api.UPDATE_UNSUCCESSFULL);
   }
 };
 
@@ -119,34 +79,24 @@ const deleteGig = async (req, res) => {
   try {
     const result = await gigService.deleteGig({ _id: req.params.gig_id });
 
-    if (result.status === "ERROR_FOUND") {
-      throw new Error("Unable to delete the Gig");
+    if (result.status === 'ERROR_FOUND') {
+      throw new Error('Unable to delete the Gig');
     }
-    if (result.status === "NOT_FOUND") {
-      return res.sendError(
-        httpCode.StatusCodes.BAD_REQUEST,
-        MESSAGES.api.NOT_FOUND
-      );
+    if (result.status === 'NOT_FOUND') {
+      return res.sendError(httpCode.StatusCodes.BAD_REQUEST, MESSAGES.api.NOT_FOUND);
     }
 
-    return res.sendSuccess(
-      result.result,
-      MESSAGES.api.SUCCESS,
-      httpCode.StatusCodes.OK
-    );
+    return res.sendSuccess(result.result, MESSAGES.api.SUCCESS, httpCode.StatusCodes.OK);
   } catch (err) {
     ErrorHandler.extractError(err);
-    return res.sendError(
-      httpCode.StatusCodes.INTERNAL_SERVER_ERROR,
-      MESSAGES.api.SOMETHING_WENT_WRONG
-    );
+    return res.sendError(httpCode.StatusCodes.INTERNAL_SERVER_ERROR, MESSAGES.api.SOMETHING_WENT_WRONG);
   }
 };
 
-router.post("/", protect, createGig);
-router.get("/:gig_id", getGig);
-router.get("/", protect, getAllGigs);
-router.put("/:gig_id", protect, updateGig);
-router.delete("/:gig_id", protect, deleteGig);
+router.post('/', protect, createGig);
+router.get('/:gig_id', getGig);
+router.get('/', protect, getAllGigs);
+router.put('/:gig_id', protect, updateGig);
+router.delete('/:gig_id', protect, deleteGig);
 
 module.exports = router;
