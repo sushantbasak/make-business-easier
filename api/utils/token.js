38bd2e2845b40c391dca6371e374b3711f9372a0
Sushant <sushantbasak73@gmail.com>
2022-02-08 14:21:24 +0530

    style: User Authentication Token Generation

--- api/utils/token.js
+++ api/utils/token.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const appSettings = require('../../config/index');
 const ErrorHandler = require('./errorHandler');
+const { ROLES } = require('../../constants');
 
 const {
   jwt: { secret, expiresIn, resetsecret, resetexpiresIn },
@@ -27,4 +28,26 @@ const generateAuthToken = async (userId, expiry = false) => {
   }
 };
 
-module.exports = { generateAuthToken };
+const generateUserAuthToken = async (userId, role = ROLES.CUSTOMER, expiry = false) => {
+  try {
+    let expiryTime = expiresIn;
+    let secretValue = secret;
+
+    if (expiry) {
+      expiryTime = resetexpiresIn;
+
+      secretValue = resetsecret;
+    }
+
+    const token = await jwt.sign({ id: userId, role, date: new Date().getTime() }, secretValue, {
+      expiresIn: expiryTime,
+    });
+
+    return { status: 'SUCCESS', token };
+  } catch (ex) {
+    ErrorHandler.extractError(ex);
+    return { status: 'ERROR_FOUND' };
+  }
+};
+
+module.exports = { generateAuthToken, generateUserAuthToken };
